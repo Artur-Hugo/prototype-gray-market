faf78d33dab17d9b4fd6b3515063e32af1c39e7e
Artur Hugo <artur.hugoliga@gmail.com>
2021-09-02 07:37:15 -0300

    primeiro commit

---
+++ src/main/java/br/com/devoliga/GraymarketApplication.java
@@ -0,0 +1,13 @@
+package br.com.devoliga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraymarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraymarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devoliga/GraymarketApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devoliga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraymarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
