38ddd8a4bf78e5e3901d595d4acc353d554514b4
Artur Hugo <artur.hugoliga@gmail.com>
2021-09-04 19:03:10 -0300

    Criando Repository e controller

--- src/main/java/br/com/devoliga/controller/CategoriaController.java
+++ src/main/java/br/com/devoliga/controller/CategoriaController.java
@@ -1,16 +1,43 @@
 package br.com.devoliga.controller;
 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.devoliga.domain.Categoria;
+import br.com.devoliga.repository.CategoriaRepository;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaController {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando!";
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Categoria>> getAll(){
+		return ResponseEntity.ok(categoriaRepository.findAll());
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> getById(@PathVariable  Long Id){
+		return categoriaRepository.findById(Id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+		
+	
+		
+		
+	
 
 }
---
+++ src/main/java/br/com/devoliga/domain/Categoria.java
@@ -0,0 +1,73 @@
+package br.com.devoliga.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+//Serializable serve para que os objetos sejam convertidos
+//em uma sequência de bytes, ou seja, pode trafegar na rede,
+//se gravados em arquivos e assim por diante.
+@Entity
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String nome;
+	
+	public Categoria() {}
+	
+	
+	public Categoria(long id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id != other.id)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
+	
+}
