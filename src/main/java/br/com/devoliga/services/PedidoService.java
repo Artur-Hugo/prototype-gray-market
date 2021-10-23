512dfce37f6108fac2e0e94d132003522c0b46fa
Artur-Hugo <Artur.Hugoliga@gmail.com>
2021-10-23 02:52:06 -0300

    Implementando toString de Pedido

--- src/main/java/br/com/devoliga/services/PedidoService.java
+++ src/main/java/br/com/devoliga/services/PedidoService.java
@@ -10,6 +10,7 @@ import br.com.devoliga.domain.ItemPedido;
 import br.com.devoliga.domain.PagamentoComBoleto;
 import br.com.devoliga.domain.Pedido;
 import br.com.devoliga.enums.EstadoPagamento;
+import br.com.devoliga.repositories.ClienteRepository;
 import br.com.devoliga.repositories.ItemPedidoRepository;
 import br.com.devoliga.repositories.PagamentoRepository;
 import br.com.devoliga.repositories.PedidoRepository;
@@ -34,6 +35,9 @@ public class PedidoService {
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
 	
+	@Autowired
+	private ClienteService clienteService;
+	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -45,6 +49,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if(obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -55,10 +60,12 @@ public class PedidoService {
 		obj = repo.save(obj);
 		for(ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
 			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 	
