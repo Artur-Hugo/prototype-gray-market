af05f171f5772615156fcabe4ad0fb9f59e29a2a
Artur-Hugo <Artur.Hugoliga@gmail.com>
2021-10-24 17:57:54 -0300

    email-html

--- src/main/java/br/com/devoliga/services/EmailService.java
+++ src/main/java/br/com/devoliga/services/EmailService.java
@@ -1,15 +1,20 @@
 package br.com.devoliga.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.stereotype.Service;
+
 
 import br.com.devoliga.domain.Pedido;
 
-@Service
+
 public interface EmailService {
 	
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
 
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
