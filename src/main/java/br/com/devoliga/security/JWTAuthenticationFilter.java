49906a4cd0c38efc73864a522009061a1e2ce762
Artur-Hugo <Artur.Hugoliga@gmail.com>
2021-10-28 15:07:37 -0300

    Arrumando o Implementando autenticação e geração do token JWT

---
+++ src/main/java/br/com/devoliga/security/JWTAuthenticationFilter.java
@@ -0,0 +1,65 @@
+package br.com.devoliga.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.devoliga.dto.CredenciaisDTO;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+
+	private AuthenticationManager authenticationManager;
+	
+	private JWTUtil jwtUtil;
+	
+	public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
+		this.authenticationManager = authenticationManager;
+		this.jwtUtil = jwtUtil;
+	}
+	
+	
+	//Metodo para Autenticação, filtro de autenticação:
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest req, 
+												HttpServletResponse res) throws AuthenticationException{
+		//Está credenciando os dados no Dto a partir dos dados das requisições
+		try {
+			CredenciaisDTO creds = new ObjectMapper()
+	                .readValue(req.getInputStream(), CredenciaisDTO.class);
+	
+	        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getSenha(), new ArrayList<>());
+	        
+	        Authentication auth = authenticationManager.authenticate(authToken);
+	        
+	        ///Esse objeto vai informar para o Spring Security se esse objeto foi com sucesso ou não
+	        return auth;
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	@Override
+	protected void successfulAuthentication(HttpServletRequest req,
+											HttpServletResponse res,
+											FilterChain chain,
+											Authentication auth) throws IOException, ServletException{
+		
+		//o auth vem do metodo authentication de cima
+		String username = ((UserSS) auth.getPrincipal()).getUsername();
+		String token = jwtUtil.generateToken(username);
+		res.addHeader("Authorization", "Bearer " + token);
+	}}
+						
