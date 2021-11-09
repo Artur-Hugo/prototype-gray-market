d6b3ddd13bf5519ae65fc1d9094d350c53173400
Artur-Hugo <Artur.Hugoliga@gmail.com>
2021-11-09 01:29:49 -0300

    Acrescentando mais produtos para testar infinity scroll

--- src/main/java/br/com/devoliga/GraymarketApplication.java
+++ src/main/java/br/com/devoliga/GraymarketApplication.java
@@ -105,6 +105,48 @@ public class GraymarketApplication implements CommandLineRunner{
 		produtoRepository.save(p8);produtoRepository.save(p9);produtoRepository.save(p10);produtoRepository.save(p11);
 		
 		
+		Produto p12 = new Produto(12, "Produto 12", 10.00);
+		Produto p13 = new Produto(13, "Produto 13", 10.00);
+		Produto p14 = new Produto(14, "Produto 14", 10.00);
+		Produto p15 = new Produto(15, "Produto 15", 10.00);
+		Produto p16 = new Produto(16, "Produto 16", 10.00);
+		Produto p17 = new Produto(17, "Produto 17", 10.00);
+		Produto p18 = new Produto(18, "Produto 18", 10.00);
+		Produto p19 = new Produto(19, "Produto 19", 10.00);
+		Produto p20 = new Produto(20, "Produto 20", 10.00);
+		Produto p21 = new Produto(21, "Produto 21", 10.00);
+		Produto p22 = new Produto(22, "Produto 22", 10.00);
+		Produto p23 = new Produto(23, "Produto 23", 10.00);
+		Produto p24 = new Produto(24, "Produto 24", 10.00);
+		Produto p25 = new Produto(25, "Produto 25", 10.00);
+		Produto p26 = new Produto(26, "Produto 26", 10.00);
+		Produto p27 = new Produto(27, "Produto 27", 10.00);
+		Produto p28 = new Produto(28, "Produto 28", 10.00);
+		Produto p29 = new Produto(29, "Produto 29", 10.00);
+		Produto p30 = new Produto(30, "Produto 30", 10.00);
+		Produto p31 = new Produto(31, "Produto 31", 10.00);
+		Produto p32 = new Produto(32, "Produto 32", 10.00);
+		Produto p33 = new Produto(33, "Produto 33", 10.00);
+		Produto p34 = new Produto(34, "Produto 34", 10.00);
+		Produto p35 = new Produto(35, "Produto 35", 10.00);
+		Produto p36 = new Produto(36, "Produto 36", 10.00);
+		Produto p37 = new Produto(37, "Produto 37", 10.00);
+		Produto p38 = new Produto(38, "Produto 38", 10.00);
+		Produto p39 = new Produto(39, "Produto 39", 10.00);
+		Produto p40 = new Produto(40, "Produto 40", 10.00);
+		Produto p41 = new Produto(41, "Produto 41", 10.00);
+		Produto p42 = new Produto(42, "Produto 42", 10.00);
+		Produto p43 = new Produto(43, "Produto 43", 10.00);
+		Produto p44 = new Produto(44, "Produto 44", 10.00);
+		Produto p45 = new Produto(45, "Produto 45", 10.00);
+		Produto p46 = new Produto(46, "Produto 46", 10.00);
+		Produto p47 = new Produto(47, "Produto 47", 10.00);
+		Produto p48 = new Produto(48, "Produto 48", 10.00);
+		Produto p49 = new Produto(49, "Produto 49", 10.00);
+		Produto p50 = new Produto(50, "Produto 50", 10.00);
+		
+
+		
 		
 	
 		cat2.getProdutos().addAll(Arrays.asList(p2, p4));
@@ -130,7 +172,58 @@ public class GraymarketApplication implements CommandLineRunner{
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
 		
-	     
+		p12.getCategorias().add(cat1);
+		p13.getCategorias().add(cat1);
+		p14.getCategorias().add(cat1);
+		p15.getCategorias().add(cat1);
+		p16.getCategorias().add(cat1);
+		p17.getCategorias().add(cat1);
+		p18.getCategorias().add(cat1);
+		p19.getCategorias().add(cat1);
+		p20.getCategorias().add(cat1);
+		p21.getCategorias().add(cat1);
+		p22.getCategorias().add(cat1);
+		p23.getCategorias().add(cat1);
+		p24.getCategorias().add(cat1);
+		p25.getCategorias().add(cat1);
+		p26.getCategorias().add(cat1);
+		p27.getCategorias().add(cat1);
+		p28.getCategorias().add(cat1);
+		p29.getCategorias().add(cat1);
+		p30.getCategorias().add(cat1);
+		p31.getCategorias().add(cat1);
+		p32.getCategorias().add(cat1);
+		p33.getCategorias().add(cat1);
+		p34.getCategorias().add(cat1);
+		p35.getCategorias().add(cat1);
+		p36.getCategorias().add(cat1);
+		p37.getCategorias().add(cat1);
+		p38.getCategorias().add(cat1);
+		p39.getCategorias().add(cat1);
+		p40.getCategorias().add(cat1);
+		p41.getCategorias().add(cat1);
+		p42.getCategorias().add(cat1);
+		p43.getCategorias().add(cat1);
+		p44.getCategorias().add(cat1);
+		p45.getCategorias().add(cat1);
+		p46.getCategorias().add(cat1);
+		p47.getCategorias().add(cat1);
+		p48.getCategorias().add(cat1);
+		p49.getCategorias().add(cat1);
+		p50.getCategorias().add(cat1);
+		
+		produtoRepository.save(p12);produtoRepository.save(p13);produtoRepository.save(p14);produtoRepository.save(p15);
+		produtoRepository.save(p16);produtoRepository.save(p17);produtoRepository.save(p18);produtoRepository.save(p19);
+		produtoRepository.save(p20);produtoRepository.save(p21);produtoRepository.save(p22);produtoRepository.save(p23);
+		produtoRepository.save(p24);produtoRepository.save(p25);produtoRepository.save(p26);produtoRepository.save(p27);
+		produtoRepository.save(p28);produtoRepository.save(p29);produtoRepository.save(p30);produtoRepository.save(p31);
+		produtoRepository.save(p32);produtoRepository.save(p33);produtoRepository.save(p34);produtoRepository.save(p35);
+		produtoRepository.save(p36);produtoRepository.save(p37);produtoRepository.save(p38);produtoRepository.save(p39);
+		produtoRepository.save(p40);produtoRepository.save(p41);produtoRepository.save(p42);produtoRepository.save(p43);
+		produtoRepository.save(p44);produtoRepository.save(p45);produtoRepository.save(p46);produtoRepository.save(p47);
+		produtoRepository.save(p48);produtoRepository.save(p49);produtoRepository.save(p50);
+		
+		
 		Estado est1 = new Estado(  1, "Minas Gerais");
 		Estado est2 = new Estado( 2, "São Paulo");
 		estadoRepository.save(est1);estadoRepository.save(est2);
